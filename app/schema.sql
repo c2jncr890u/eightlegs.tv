dd4223dae9f96fb157a2a0ce979594d741a69a70
Drew <c2jncr890u@gmail.com>
2011-01-26 05:08:20 -0500

    Created a very minimal database schema that should work for MVP.

---
+++ app/schema.sql
@@ -0,0 +1,27 @@
+
+SET SESSION storage_engine = "InnoDB";
+SET SESSION time_zone = "+0:00";
+ALTER DATABASE CHARACTER SET "utf8";
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    u VARCHAR(128) NOT NULL UNIQUE,
+    pwdhash VARCHAR(128)
+);
+
+CREATE TABLE queries (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    q VARCHAR(128) NOT NULL UNIQUE
+);
+
+CREATE TABLE videos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    v VARCHAR(11) NOT NULL UNIQUE
+);
+
+CREATE TABLE playlists (
+    uid INT NOT NULL REFERENCES users(id),
+    qid INT NOT NULL REFERENCES queries(id),
+    vid INT NOT NULL REFERENCES videos(id),
+    PRIMARY KEY (uid,qid,vid) -- PK + find queries by (user) + find videos by (user,tag)
+);
