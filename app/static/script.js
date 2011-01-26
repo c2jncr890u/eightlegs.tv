0eb682a362135cb1b39e78d0119a6f7ef2ea96fa
Drew <c2jncr890u@gmail.com>
2011-01-26 08:34:15 -0500

    Added js file for segue into next video.

--- app/static/script.js
+++ app/static/script.js
@@ -1,22 +1,20 @@
 function skip()
-{
-    var player = document.getElementById("player-object");    
-    var v = new RegExp("[\\?&]v=([^&#]*)").exec(player.getVideoUrl())[1];
+{   
+    var v = new RegExp("[\\?&]v=([^&#]*)").exec(ytplayer.getVideoUrl())[1];
     window.location = "/?v="+v+"&q="+encodeURIComponent(document.getElementById("q").value);
 }
 function maybeskip()
 {
-    var player = document.getElementById("player-object");
-    if(player.getPlayerState()==0) skip();
+    if(ytplayer.getPlayerState()==0) skip();
 }
 function onYouTubePlayerReady(playerid)
 {
-    var player = document.getElementById("player-object");
-    player.addEventListener("onError", "skip" );
-    player.addEventListener("onStateChange", "maybeskip" );
+    var ytplayer = document.getElementById("ytplayer");
+    ytplayer.addEventListener("onError", "skip" );
+    ytplayer.addEventListener("onStateChange", "maybeskip" );
 }
 function rate( rating ){
-    var v = new RegExp("[\\?&]v=([^&#]*)").exec(player.getVideoUrl())[1];
+    var v = new RegExp("[\\?&]v=([^&#]*)").exec(ytplayer.getVideoUrl())[1];
     $.post("/rate",
         { 
             q: document.getElementById("q").value,
