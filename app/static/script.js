6526e8e9c598b1532f5fc6e2d99a6f8e468661b9
Drew <c2jncr890u@gmail.com>
2011-01-26 08:15:59 -0500

    Added js file for segue into next video.

--- app/static/script.js
+++ app/static/script.js
@@ -1,29 +1,31 @@
 function skip()
 {
+    var player = document.getElementById("player-object");    
     var v = new RegExp("[\\?&]v=([^&#]*)").exec(player.getVideoUrl())[1];
     window.location = "/?v="+v+"&q="+encodeURIComponent(document.getElementById("q").value);
 }
 function maybeskip()
 {
-   if(player.getPlayerState()==0) skip();
+    var player = document.getElementById("player-object");
+    if(player.getPlayerState()==0) skip();
 }
 function onYouTubePlayerReady(playerid)
 {
-   player = document.getElementById("player-object");
-   player.addEventListener("onError", "skip" );
-   player.addEventListener("onStateChange", "maybeskip" );
+    var player = document.getElementById("player-object");
+    player.addEventListener("onError", "skip" );
+    player.addEventListener("onStateChange", "maybeskip" );
 }
 function rate( rating ){
-   var v = new RegExp("[\\?&]v=([^&#]*)").exec(player.getVideoUrl())[1];
-   $.post("/rate",
-      { 
-         q: document.getElementById("q").value,
-         v: v,
-         r: rating
-      },
-      function () {
-         if(rating==1) document.getElementById("liked").style.visibility="visible";
-         else document.getElementById("liked").style.visibility="hidden";
-      }
-   );
+    var v = new RegExp("[\\?&]v=([^&#]*)").exec(player.getVideoUrl())[1];
+    $.post("/rate",
+        { 
+            q: document.getElementById("q").value,
+            v: v,
+            r: rating
+        },
+        function () {
+            if(rating==1) document.getElementById("liked").style.visibility="visible";
+            else document.getElementById("liked").style.visibility="hidden";
+        }
+    );
 }
